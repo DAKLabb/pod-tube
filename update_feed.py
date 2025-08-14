feb89e6858810f820501db3956140f91180407a2
David Konigsberg <72822263+davidkonigsberg@users.noreply.github.com>
2025-08-14 10:08:12 -0400

    add timestamp to rss feed

--- update_feed.py
+++ update_feed.py
@@ -1,5 +1,6 @@
 import xml.etree.ElementTree as ET
 import argparse
+from email.utils import formatdate
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Update the provided RSS feed with a new episode.")
@@ -8,6 +9,7 @@ if __name__ == "__main__":
     parser.add_argument("--url", type=str, required=True, help="URL of the file being added")
     parser.add_argument("--bytes", type=int, required=True, help="Size of the file (in bytes)")
     parser.add_argument("--guid", type=str, required=True, help="guid for the file)")
+    parser.add_argument("--timestamp", type=int, required=True, help="guid for the file)")
 
     args = parser.parse_args()
 
@@ -24,6 +26,8 @@ if __name__ == "__main__":
     enclosure_element.set("length", str(args.bytes))
     guid_element = ET.SubElement(new_item, 'guid')
     guid_element.text = args.guid
+    date_element = ET.SubElement(new_item, 'pubDate')
+    date_element.text = formatdate(args.timestamp, usegmt=True) 
 
     # Save the changes
     tree.write(args.path)
